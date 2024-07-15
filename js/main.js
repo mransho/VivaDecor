1bc6c82758e282c692fa290ff6b1cfeb8e8b153f
mahmod mostafa <94807913+mransho@users.noreply.github.com>
2024-07-15 23:50:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// let Counters = document.getElementById("Counters");
+// let Counter = document.createElement("div");
+// let CounterNum = document.createElement("div");
+// let Countertext = document.createElement("div");
+// CounterNum.textContent = "+400"
+// Countertext.textContent = "Project Complete"
+// Counters.appendChild(Counter).appendChild(CounterNum);
+// Counters.appendChild(Counter).appendChild(CounterNum);
